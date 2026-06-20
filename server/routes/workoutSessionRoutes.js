e128d55c605502d9dc219ae141e3f5ebbfa6407c
ZiadGamall <ziadgamal497@gmail.com>
2026-06-20 14:23:59 +0300

    Change workoutController and workoutRoutes to workoutSessionController and workoutSessionRoutes

--- server/routes/workoutSessionRoutes.js
+++ server/routes/workoutSessionRoutes.js
@@ -1,29 +1,29 @@
 const express = require("express");
 const authController = require("../controllers/authController");
-const workoutController = require("../controllers/workoutController");
+const workoutSessionController = require("../controllers/workoutSessionController");
 
 const router = express.Router();
 
 router
   .route("/sessions")
-  .get(authController.protect, workoutController.getWorkoutSessions)
-  .post(authController.protect, workoutController.addWorkoutSession);
+  .get(authController.protect, workoutSessionController.getWorkoutSessions)
+  .post(authController.protect, workoutSessionController.addWorkoutSession);
 
 router.patch(
   "/sessions/:id/toggle",
   authController.protect,
-  workoutController.toggleWorkoutCompleted,
+  workoutSessionController.toggleWorkoutCompleted,
 );
 router.delete(
   "/sessions/:id",
   authController.protect,
-  workoutController.deleteWorkoutSession,
+  workoutSessionController.deleteWorkoutSession,
 );
 
 router.get(
   "/progress/weekly",
   authController.protect,
-  workoutController.getWeeklyProgress,
+  workoutSessionController.getWeeklyProgress,
 );
 
 module.exports = router;
